609c6413da7cddfd1675caf74175cf096fa3b864
ToniCoffee <ntnlpzsgr@gmail.com>
2022-06-01 21:46:27 +0200

    try fix project indentation

--- components/avatar/index.js
+++ components/avatar/index.js
@@ -2,12 +2,12 @@ import Image from 'next/image';
 import styles from './style.module.css';
 
 export default function Avatar({src, alt, text, width=50, height=50, column}) {
-	return (
-		<div className={column ? (styles.avatar + '\n' + styles.column) : (styles.avatar + '\n' + styles.row)}>
-			<div className={styles.avatarImgContainer} >
-				<Image className={styles.avatarImg} width={width} height={height} src={src} alt={alt} />
-			</div>
-			{text && <strong>{text}</strong>}
-		</div>
-	);
+  return (
+    <div className={column ? (styles.avatar + '\n' + styles.column) : (styles.avatar + '\n' + styles.row)}>
+      <div className={styles.avatarImgContainer} >
+        <Image className={styles.avatarImg} width={width} height={height} src={src} alt={alt} />
+      </div>
+      {text && <strong>{text}</strong>}
+    </div>
+  );
 }
